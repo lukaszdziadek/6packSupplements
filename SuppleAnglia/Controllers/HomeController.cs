b91a590da9c7c8a703f95a631cbc69e9fba392f1
lukaszdziadek <lukaszdziadek.jaw@gmail.com>
2015-10-21 13:53:23 +0200

    test commita
    //test

--- SuppleAnglia/Controllers/HomeController.cs
+++ SuppleAnglia/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SuppleAnglia.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description pagee.";
 
             return View();
         }
