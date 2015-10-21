df52cf1390ef22bdd6f443d6924088cd364e6cb5
lukaszdziadek <lukaszdziadek.jaw@gmail.com>
2015-10-21 13:37:48 +0200

    przeniesienie projektu na gita

---
+++ SuppleAnglia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuppleAnglia.MvcApplication" Language="C#" %>
---
+++ SuppleAnglia/Scripts/asideMenu.js
@@ -0,0 +1,48 @@
+﻿
+
+$(function(){
+
+    $("#lista").addClass("zamkniete");
+
+    var mainCategory = $("#lista");
+
+
+
+
+    //$("#lista > li").click(function (event) {
+    //    event.stopPropagation();
+    //    alert("hello LI pierwszy");
+    //});
+
+    $("#pierwszyDzial").click(function () {
+        alert("kliknales li pierwszy");
+
+        $("#pierwszyDzial > ul").toggle("blind",500);
+    });
+
+
+    $("#lista li ul > li").click(function (event) {
+
+       
+       
+        if (mainCategory.hasClass("zamkniete")) {
+            alert("Asdsad");
+            event.stopPropagation();
+            alert(event.isPropagationStopped());
+
+        }
+        else {
+            //mainCategory.removeClass("otwarte");
+            //mainCategory.addClass("zamkniete");
+        };
+
+
+    });
+
+
+    //$("#lista li:first-child").click(function (event){
+    //    event.stopPropagation();
+    //});
+
+});
+
